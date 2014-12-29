76fd72d9c4082c0a5c8f3ab696e3bb7dc29ab17f
Zachary Chavez <zach@rychavez.com>
2014-12-29 10:01:32 -0700

    Refs #1 - Implement browserify workaround for Phaser.

--- application/src/main.js
+++ application/src/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // https://github.com/photonstorm/phaser/issues/1186
-var Phaser = require('phaser');
+var Phaser = require('./phaser');
 
 // var game = new Phaser.Game(800, 600, Phaser.AUTO, undefined, { preload : preload, create : create});
 
